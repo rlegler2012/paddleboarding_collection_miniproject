7587237c9348ca9e101c31f152b9d4c4db235c7f
rlegler2012 <rlegler2012@hotmail.com>
2023-07-03 11:35:11 -0700

    corrected about,home views working

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import View 
+from django.http import HttpResponse # <- a class to handle sending a type of response
+from django.views.generic import TemplateView
 
-# Create your views here.
+class Home(TemplateView):
+    template_name = "home.html"
+    
+
+class About(TemplateView):
+    template_name = "about.html"
