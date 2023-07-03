b3022dce105943a24fe9afe31353631462577eb2
rlegler2012 <rlegler2012@hotmail.com>
2023-07-03 11:42:13 -0700

    server running and about,home pages working

---
+++ main_app/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.Home.as_view(), name="home"), # <- here we have added the new path
+    path('about/', views.About.as_view(), name="about"),
+]
