0969fc79e34c6042ee1ec8b58c641d1e022095a9
rlegler2012 <rlegler2012@hotmail.com>
2023-07-05 11:03:48 -0700

    location_list route working with random seed data

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.Home.as_view(), name="home"), # <- here we have added the new path
+    path('', views.Home.as_view(), name="home"),
     path('about/', views.About.as_view(), name="about"),
+    path('locations/', views.LocationList.as_view(), name="location_list")
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -9,3 +9,26 @@ class Home(TemplateView):
 
 class About(TemplateView):
     template_name = "about.html"
+
+ #adds artist class for mock database data
+class Location:
+    def __init__(self, name, image, description):
+        self.name = name
+        self.image = image
+        self.description = description
+
+
+locations = [
+  Location ("Little Spokane River", "https://assets.change.org/photos/9/lq/sr/HuLQsrEXlyAdApc-1600x900-noPad.jpg?1622402845",
+          "Blah"),
+  Location ("Heyburn State Park",
+          "https://lh3.googleusercontent.com/p/AF1QipOheBfE-Bixtqcbm3zw5N_wif3JiDaAzkiKEb1I=s1360-w1360-h1020", "lake"),
+]
+
+class LocationList(TemplateView):
+    template_name = "location_list.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["locations"] = locations # this is where we add the key into our context object for the view to use
+        return context
